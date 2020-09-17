e5b472e05f8b866b1770e3e6cef34a87a004862c
Nicolas Martin Leidi <nicolasmleidi@gmail.com>
2020-09-17 08:32:01 -0300

    Especifico mejor los loggings

--- src/Graph.java
+++ src/Graph.java
@@ -25,13 +25,13 @@ public class Graph {
 		if(node >= 1) {
 			if (mapNodos.get(node) == null) {
 				mapNodos.put(node, new Nodo());
-				logger.fine("Agregué nodo: " + node);
+				logger.fine("Agregué un nodo: " + node);
 			}
 			else
-				logger.warning("Nodo introducido ya existe");
+				logger.warning("No se pudo agregar un nodo, nodo introducido ya existe");
 		}
 		else
-			logger.warning("Nodo introducido no válido");
+			logger.warning("No se pudo agregar un nodo, nodo introducido no válido");
 	}
 	
 	public void addEdge(int node1, int node2) {
@@ -39,13 +39,13 @@ public class Graph {
 			Nodo n = mapNodos.get(node1);
 			if(n != null && mapNodos.get(node2) != null) {
 				n.addAdy(node2);
-				logger.fine("Agregué arco: " + node1 + " con " + node2);
+				logger.fine("Agregué un arco: " + node1 + " con " + node2);
 			}
 			else
-				logger.warning("Nodos introducidos no pertenecen al grafo");
+				logger.warning("No se pudo agregar un arco, nodos introducidos no pertenecen al grafo");
 		}
 		else
-			logger.warning("Nodos introducidos no válidos");
+			logger.warning("No se pudo agregar un arco, nodos introducidos no válidos");
 	}
 	
 	public void removeNode(int node) {
@@ -54,13 +54,13 @@ public class Graph {
 				mapNodos.remove(node);
 				for(Nodo n : mapNodos.values())
 					n.removeAdy(node);
-				logger.fine("Removí nodo: " + node);
+				logger.fine("Removí un nodo: " + node);
 			}
 			else
-				logger.warning("Nodos introducidos no pertenecen al grafo");
+				logger.warning("No se pudo removar un nodo, nodo introducido no pertenece al grafo");
 		}
 		else
-			logger.warning("Nodo introducido no válido");
+			logger.warning("No se pudo removar un nodo, nodo introducido no válido");
 	}
 	
 	public void removeEdge(int node1, int node2) {
@@ -72,12 +72,12 @@ public class Graph {
 					logger.fine("Removí arco: " + node1 + " con " + node2);
 				}
 				else
-					logger.warning("No pude remover arco: " + node1 + " con " + node2);
+					logger.warning("No pude remover arco, no existe arco que conecte a " + node1 + " con " + node2);
 			}
 			else
-				logger.warning("Nodos introducidos no pertenecen al grafo");
+				logger.warning("No pude remover arco, nodos introducidos no pertenecen al grafo");
 		}
 		else
-			logger.warning("Nodos introducidos no válidos");
+			logger.warning("No pude remover arco, nodos introducidos no válidos");
 	}
 }
